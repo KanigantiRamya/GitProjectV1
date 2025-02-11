36fa74f68544d007a6261e6b29d0b90642d05b34
KanigantiRamya <kanigantiramyaramya@gmail.com>
2025-02-11 13:35:17 +0530

    This is my first comment

---
+++ src/Login.java
@@ -0,0 +1,17 @@
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "liers";
+		
+		if (name == "liers")
+			System.out.println("Dont trust....");
+		else {
+			System.out.println("Do work....");
+		}
+			
+
+	}
+
+}
