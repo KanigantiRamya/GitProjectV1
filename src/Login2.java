387ac1998037b58179d2138d89fe58f1f5ed0fbe
KanigantiRamya <kanigantiramyaramya@gmail.com>
2025-03-27 12:54:15 +0530

    Implement new UI for dashboard

---
+++ src/Login2.java
@@ -0,0 +1,17 @@
+
+public class Login2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "liers";
+		
+		if (name == "liers")
+			System.out.println("Dont trust any one blindly.....");
+		else {
+			System.out.println("trust every one...");
+		}
+			
+
+	}
+
+}
